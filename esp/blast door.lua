7808d59ac9e4eb600ffe3276ec1abbfc755f3171
zazu197 <jarlestromliam@gmail.com>
2026-07-01 21:17:29 +0200

    Implement highlight for active LockdownGate models
    Added highlight functionality for active lockdown gates.

--- esp/blast door.lua
+++ esp/blast door.lua
@@ -4,9 +4,28 @@ local Descendants = GatesFolder:GetDescendants()
 for _, item in Descendants do
     if item:IsA("Model") and item.Name == "LockdownGate" then
         if item:GetAttribute("Active") == true then
-            print(item:GetPivot() )
-            print("Gate is active")
+            local gatehighlight = Instance.new("Highlight")
+            gatehighlight.Adornee = item
+            gatehighlight.Parent = item
+        else
+            local oldHighlight = item:FindFirstChildWhichIsA("Highlight")
+            if oldHighlight then
+                oldHighlight:Destroy()
+            end
         end
+        item:GetAttributeChangedSignal("Active"):Connect(function()
+        if item:GetAttribute("Active") == true then
+            local gatehighlight = Instance.new("Highlight")
+            gatehighlight.Adornee = item
+            gatehighlight.Parent = item
+        else
+            local oldHighlight = item:FindFirstChildWhichIsA("Highlight")
+            if oldHighlight then
+                oldHighlight:Destroy()
+            end
+        end
+        end)
+        
     end
 end
 
